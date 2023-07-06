56f946afbcb2b202b3bb26c8290d3623a1ee18a5
Dominikus Baur <dominikus.baur@gmail.com>
2023-07-06 11:56:36 +0200

    add globe-texture-generator

---
+++ globe-texture-generator/README.md
@@ -0,0 +1,6 @@
+# globe texture gen
+
+Use this as an example for how to generate new textures for the globe.
+
+## how to use
+Just copy-paste goal06.html into a new html file (also needs a local webserver, e.g., `npx live-server`), load your own data and then right-click on 'save' in Chrome on the canvas to get a texture. Store that texture in the main repository as: /static/assets/textures/goalXX.png
---
+++ globe-texture-generator/circle-to-polygon.js
@@ -0,0 +1,104 @@
+const defaultEarthRadius = 6378137; // equatorial Earth radius
+
+function toRadians(angleInDegrees) {
+  return (angleInDegrees * Math.PI) / 180;
+}
+
+function toDegrees(angleInRadians) {
+  return (angleInRadians * 180) / Math.PI;
+}
+
+function offset(c1, distance, earthRadius, bearing) {
+  var lat1 = toRadians(c1[1]);
+  var lon1 = toRadians(c1[0]);
+  var dByR = distance / earthRadius;
+  var lat = Math.asin(
+    Math.sin(lat1) * Math.cos(dByR) +
+      Math.cos(lat1) * Math.sin(dByR) * Math.cos(bearing)
+  );
+  var lon =
+    lon1 +
+    Math.atan2(
+      Math.sin(bearing) * Math.sin(dByR) * Math.cos(lat1),
+      Math.cos(dByR) - Math.sin(lat1) * Math.sin(lat)
+    );
+  return [toDegrees(lon), toDegrees(lat)];
+}
+
+function circleToPolygon(center, radius, options) {
+  var n = getNumberOfEdges(options);
+  var earthRadius = getEarthRadius(options);
+  var bearing = getBearing(options);
+  var direction = getDirection(options);
+
+  // validateInput() throws error on invalid input and do nothing on valid input
+  // validateInput({ center, radius, numberOfEdges: n, earthRadius, bearing });
+
+  var start = toRadians(bearing);
+  var coordinates = [];
+  for (var i = 0; i < n; ++i) {
+    coordinates.push(
+      offset(
+        center,
+        radius,
+        earthRadius,
+        start + (direction * 2 * Math.PI * -i) / n
+      )
+    );
+  }
+  coordinates.push(coordinates[0]);
+
+  return {
+    type: "Polygon",
+    coordinates: [coordinates],
+  };
+}
+
+function getNumberOfEdges(options) {
+  if (isUndefinedOrNull(options)) {
+    return 32;
+  } else if (isObjectNotArray(options)) {
+    var numberOfEdges = options.numberOfEdges;
+    return numberOfEdges === undefined ? 32 : numberOfEdges;
+  }
+  return options;
+}
+
+function getEarthRadius(options) {
+  if (isUndefinedOrNull(options)) {
+    return defaultEarthRadius;
+  } else if (isObjectNotArray(options)) {
+    var earthRadius = options.earthRadius;
+    return earthRadius === undefined ? defaultEarthRadius : earthRadius;
+  }
+  return defaultEarthRadius;
+}
+
+function getDirection(options) {
+  if (isObjectNotArray(options) && options.rightHandRule) {
+    return -1;
+  }
+  return 1;
+}
+
+function getBearing(options) {
+  if (isUndefinedOrNull(options)) {
+    return 0;
+  } else if (isObjectNotArray(options)) {
+    var bearing = options.bearing;
+    return bearing === undefined ? 0 : bearing;
+  }
+  return 0;
+}
+
+function isObjectNotArray(argument) {
+  return (
+    argument !== null &&
+    typeof argument === "object" &&
+    !Array.isArray(argument)
+  );
+}
+
+function isUndefinedOrNull(argument) {
+  return argument === null || argument === undefined;
+}
